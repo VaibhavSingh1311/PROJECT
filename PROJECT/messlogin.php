dde99651c3bb9c37d19b7c96f135d0979b07c7dc
MrinalSharma87 <125037134+MrinalSharma87@users.noreply.github.com>
2023-03-20 17:32:29 +0530

    Update messlogin.php

--- PROJECT/messlogin.php
+++ PROJECT/messlogin.php
@@ -8,8 +8,6 @@ $dbname = "testdatabase";
 
    $email_address = $_POST['email_address'];
     $password2 = $_POST['password'];
-    // $confirm_password = $_POST['confirm_password'];
-    // $conn = mysqli_connect('localhost','root','','test2');
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -41,21 +39,5 @@ if ($result->num_rows > 0) {
 
 // Close connection
 $conn->close();
-
-   //  $email_address = $_POST['email_address']
-   //  $password = $_POST['password']
-   //  $confirm_password = $_POST['confirm_password']
-   //  $conn = new mysqli('localhost','root','','test');
-   //  if($conn->connect_error){
-   //      die('Connection Failed : '.$conn->connect_error);
-   //  }else{
-   //      $stmt = $conn->prepare("insert into registration(email_address, password, confirm_password)values(?, ?, ?)");
-   //      $stmt->bind_param("sss",$email_address, $password, $confirm_password);
-   //      $stmt->execute();
-   //      echo "registration successful..";
-   //      $stmt->close();
-   //      $conn->close();
-   //  }
-
 
 ?>
